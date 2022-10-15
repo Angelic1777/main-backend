b83621249cc0cb6d3a4041aa30222ddaa7b920a5
Angelic D <66695721+Angelic1777@users.noreply.github.com>
2022-10-14 18:10:18 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
-app.get('/api/user', (req, res) => {
-  res.send('We are home page')
-})
+app.get('/', function (req, res) { res.send('We are HomePage')})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(3000)
